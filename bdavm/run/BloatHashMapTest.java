5d72f311fb28ca749ed7e79bd15e6dc9289f05c9
Duarte Patrício <duarte.carvalho@tecnico.ulisboa.pt>
2016-10-19 00:33:54 +0100

    Remade the microbenchmark. Changed the option to select the libjvm.so/dylib directory to a more obvious one.

--- bdavm/run/BloatHashMapTest.java
+++ bdavm/run/BloatHashMapTest.java
@@ -1,14 +1,13 @@
+import java.util.HashMap;
 /*
  * Fills a big hashmap with tons of entries, and then tries to compute
  * new hash values for it
  */
 public class BloatHashMapTest {
 
-  public static MyHashMap<Long, String> map;
-  public static MyHashMap<Long, String> map1;
-  public static MyHashMap<Long, String> map2;
-  // public static HashMap<Long, String> map3;
-  // public static HashMap<Long, String> map4;
+  public static int readCount = 1000000000;
+  public static HashMap<Long, String> map;
+  public static HashMap<Long, String> map1;
 
   public static void main(String[] args) throws Exception {
     System.out.println("--------------- BloatHashMapTest STARTED ---------------");
@@ -16,19 +15,59 @@ public class BloatHashMapTest {
     Bootstrap bs  = new Bootstrap();
     int gc_count = 0;
     map = bs.fillMapRandom();
-    map2 = bs.fillMapRandom();
-    System.out.println("-- Invoking GC -- count = " + gc_count++);
-    System.gc();
-    map1 = bs.fillMapValues();
-    System.out.println("-- Invoking GC -- count = " + gc_count++);
-    System.gc();
+    map1 = bs.fillMapRandom();
+    HashMap<Long, String> map2 = bs.getMap2();
+    HashMap<Long, String> map3 = bs.getMap3();
+    HashMap<Long, String> map4 = bs.getMap4();
+    HashMap<Long, String> map5 = bs.getMap5();
+    
+    // Read values on and on and report time taken
 
+    System.out.println("---- Starting to read ----");
+    long startTime = System.nanoTime();
 
-    for (String s : map1.values()) {
-      // Duarte overriden by Cândida
-      if (s.equals("Ivan") | s.equals("Duarte") | s.equals("Cândida"))
-        System.out.println(s);
+    int j;
+    for (int i = 0; i < readCount; ++i) {
+      j = i % bs.max;
+      if (map.get(j) != null) {
+        map.put((long)j, "Value scanned");
+      }
     }
+    for (int i = 0; i < readCount; ++i) {
+      j = i % bs.max;
+      if (map1.get(j) != null) {
+        map1.put((long)j, "Value scanned");
+      }
+    }
+    for (int i = 0; i < readCount; ++i) {
+      j = i % bs.max;
+      if (map2.get(j) != null) {
+        map2.put((long)j, "Value scanned");
+      }
+    }
+    for (int i = 0; i < readCount; ++i) {
+      j = i % bs.max;
+      if (map3.get(j) != null) {
+        map3.put((long)j, "Value scanned");
+      }
+    }
+    for (int i = 0; i < readCount; ++i) {
+      j = i % bs.max;
+      if (map4.get(j) != null) {
+        map4.put((long)j, "Value scanned");
+      }
+    }
+    for (int i = 0; i < readCount; ++i) {
+      j = i % bs.max;
+      if (map5.get(j) != null) {
+        map5.put((long)j, "Value scanned");
+      }
+    }
+
+    long elapsedTime = System.nanoTime() - startTime;
+    double divisor = 1000000000.0;
+    System.out.println("---- Finished reading in " + elapsedTime / divisor + " seconds. ----");
+    
 
     System.out.println("--------------- BloatHashMapTest DONE ---------------");
   }
--- bdavm/run/Bootstrap.java
+++ bdavm/run/Bootstrap.java
@@ -1,93 +1,81 @@
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Base64;
 
 public class Bootstrap {
 
-    // We'll have to try with Integer.MAX_VALUE, but with a bigger heap
-    // to prevent OutOfMemory errors.
-    private static int max = 1000000;
+  // We'll have to try with Integer.MAX_VALUE, but with a bigger heap
+  // to prevent OutOfMemory errors.
+  public static int max = 25000;
 
-    private MyHashMap<Long, String> map;
-    private MyHashMap<Long, String> map1;
-    private MyHashMap<Long, String> map2;
-    private MyHashMap<Long, String> map3;
-    private MyHashMap<Long, String> map4;
-    private MyHashMap<Long, String> map5;
-    private int map_counts = 0;
+  private HashMap<Long, String> map;
+  private HashMap<Long, String> map1;
+  private HashMap<Long, String> map2;
+  private HashMap<Long, String> map3;
+  private HashMap<Long, String> map4;
+  private HashMap<Long, String> map5;
+  private int map_counts = 0;
 
-    private MyHashMap<Long, String> ConstructorHelper(int sz) {
-        MyHashMap<Long, String> m = new MyHashMap<Long, String>(sz);
-        System.out.println("\nCreated map number " + ++map_counts);
-        return m;
+  public HashMap<Long, String> getMap0() { return map; }
+  public HashMap<Long, String> getMap1() { return map1; }
+  public HashMap<Long, String> getMap2() { return map2; }
+  public HashMap<Long, String> getMap3() { return map3; }
+  public HashMap<Long, String> getMap4() { return map4; }
+  public HashMap<Long, String> getMap5() { return map5; }
+  
+  private HashMap<Long, String> ConstructorHelper(int sz) {
+    HashMap<Long, String> m = new HashMap<Long, String>(sz);
+    System.out.println("\nCreated map number " + ++map_counts);
+    return m;
+  }
+
+  public Bootstrap() {
+    map = ConstructorHelper(max);
+    fillHashMapWithRandom(map);
+    map1 = ConstructorHelper(max);
+    fillHashMapWithRandom(map1);
+    map2 = ConstructorHelper(max);
+    fillHashMapWithRandom(map2);
+    map3 = ConstructorHelper(max);
+    fillHashMapWithRandom(map3);
+    map4 = ConstructorHelper(max);
+    fillHashMapWithRandom(map4);
+    map5 = ConstructorHelper(max);
+    fillHashMapWithRandom(map5);
+  }
+
+  public HashMap<Long, String> fillMapRandom() {
+    fillHashMapWithRandom(map);
+    return map;
+  }
+
+  public HashMap<Long, String> fillMapValues() {
+    fillHashMap(map1, map);
+    return map1;
+  }
+
+  private void fillHashMapWithRandom(HashMap<Long, String> map) {
+    if(map == null)
+      return;
+
+    Random generator = new Random((long)max);
+    Base64.Encoder encoder = Base64.getEncoder();
+    for(int i = 0; i < max; i++) {
+      long key = generator.nextInt();
+      byte[] stringBytes = new byte[6];
+      generator.nextBytes(stringBytes);
+
+      String value = encoder.encodeToString(stringBytes);
+
+      map.put(key, value);
     }
+  }
 
-    public Bootstrap() {
-        map = ConstructorHelper(max);
-        fillHashMapWithRandom(map);
-        map1 = ConstructorHelper(max * 2);
-        fillHashMapWithRandom(map1);
-        map2 = ConstructorHelper(max * 7);
-        fillHashMapWithRandom(map2);
-        map3 = ConstructorHelper(max * 6);
-        fillHashMapWithRandom(map3);
-        map4 = ConstructorHelper(max * 3);
-        fillHashMapWithRandom(map4);
-        map5 = ConstructorHelper(max * 4);
-        fillHashMapWithRandom(map5);
-    }
-
-    public MyHashMap<Long, String> fillMapRandom() {
-        fillHashMapWithRandom(map);
-        return map;
-    }
-
-  // public void fillHashMapRandom(HashMap<Long, String> m) {
-  //   // Init the map if not already
-  //   if (m == null)
-  //     m = new HashMap<Long, String>(max);
-
-  //   Random generator = new Random((long)max);
-  //   Base64.Encoder encoder = Base64.getEncoder();
-  //   for (int i = 0; i < max; i++) {
-  //     long key = generator.nextInt();
-  //     byte[] stringBytes = new byte[8];
-  //     generator.nextBytes(stringBytes);
-  //     String value = encoder.encodeToString(stringBytes);
-  //     m.put(key, value);
-  //   }
-  // }
-   
-
-    public MyHashMap<Long, String> fillMapValues() {
-        fillHashMap(map1, map);
-        return map1;
-    }
-
-    private void fillHashMapWithRandom(MyHashMap<Long, String> map) {
-        if(map == null)
-            return;
-
-        Random generator = new Random((long)max);
-        Base64.Encoder encoder = Base64.getEncoder();
-        for(int i = 0; i < max; i++) {
-            long key = generator.nextInt();
-            byte[] stringBytes = new byte[6];
-            generator.nextBytes(stringBytes);
-
-            String value = encoder.encodeToString(stringBytes);
-
-            map.myput(key, value);
-
-            if(i < 50)
-                System.out.print(" " + map.get(key) + " ");
-        }
-    }
-
-    private void fillHashMap(MyHashMap<Long, String> map1,
-                             MyHashMap<Long, String> map2) {
-        map1.putAll(map2);
-        map1.put(27L, "Duarte");
-        map1.put(42L, "Ivan");
-        map1.put(27L, "Cândida"); // override
-    }
+  private void fillHashMap(HashMap<Long, String> map1,
+                           HashMap<Long, String> map2) {
+    map1.putAll(map2);
+    map1.put(27L, "Duarte");
+    map1.put(42L, "Ivan");
+    map1.put(27L, "Cândida"); // override
+  }
 }
