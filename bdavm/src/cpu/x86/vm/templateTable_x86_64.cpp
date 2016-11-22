f5ba68eef7c00b87564c65b47105af594b14b685
Duarte Patrício <duarte.carvalho@tecnico.ulisboa.pt>
2016-11-22 11:59:53 +0000

    Cleaned some commented debug code.

--- bdavm/src/cpu/x86/vm/templateTable_x86_64.cpp
+++ bdavm/src/cpu/x86/vm/templateTable_x86_64.cpp
@@ -3379,11 +3379,6 @@ void TemplateTable::_new() {
     // Test if it is a valid BDA value
     // Must be done here after having the object allocated but before rsi
     // is consumed when storing the klass field
-
-    // int i = counter;
-    // Atomic::inc(&counter);
-    // tty->print_cr("Allocation _new called " i " times");
-    //
     Label done_enqueue;
     // Save registers
     __ push (rsi);
